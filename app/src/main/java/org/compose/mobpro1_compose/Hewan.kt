d7d1fdba883d71e10e602b045b9830f403c94821
Abdul Mughny Raihanp <abdulmughnyraihan@gmail.com>
2024-08-20 09:50:28 +0700

    Manambahkan Tampilan Galeri Hewan

---
+++ app/src/main/java/org/compose/mobpro1_compose/Hewan.kt
@@ -0,0 +1,8 @@
+package org.compose.mobpro1_compose
+
+import androidx.annotation.DrawableRes
+
+data class Hewan(
+    val nama: String,
+    @DrawableRes val imagesResId: Int
+)
--- app/src/main/java/org/compose/mobpro1_compose/MainActivity.kt
+++ app/src/main/java/org/compose/mobpro1_compose/MainActivity.kt
@@ -4,8 +4,15 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,25 +21,46 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.compose.mobpro1_compose.ui.theme.Mobpro1composeTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val data = getData()
+    private var index by mutableStateOf(0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Mobpro1composeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GaleriHewan(data[index]) {
+                        index = if (index == data.size-1) 0 else index+1
+                    }
                 }
             }
         }
+    }
+
+    private fun getData() :List<Hewan> {
+        return  listOf(
+            Hewan("Ayam", R.drawable.ayam),
+            Hewan("Bebek", R.drawable.bebek),
+            Hewan("Domba", R.drawable.domba),
+            Hewan("Kambing", R.drawable.kambing),
+            Hewan("Sapi", R.drawable.sapi),
+        )
     }
 }
 @OptIn(ExperimentalMaterial3Api::class)
@@ -54,13 +82,38 @@ fun MainScreen(content: @Composable (Modifier) -> Unit) {
         content(Modifier.padding(padding))
     }
 }
+
 @Composable
-fun Greeting(name: String) {
+fun GaleriHewan(hewan: Hewan, onClick: () -> Unit = {}) {
     MainScreen { modifier ->
-        Text(
-            text = "Hello $name!",
+        Column(
             modifier = modifier
-        )
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(hewan.imagesResId),
+                contentDescription = stringResource(R.string.gambar, hewan.nama),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(132.dp)
+            )
+            Text(
+                text = hewan.nama,
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Button(
+                onClick = { onClick() },
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(top = 24.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                Text(text = stringResource(R.string.lanjut))
+            }
+        }
     }
 }
 
@@ -69,6 +122,6 @@ fun Greeting(name: String) {
 @Composable
 fun ScreenPreview() {
     Mobpro1composeTheme {
-        Greeting("Android")
+        GaleriHewan(Hewan("Ayam", R.drawable.ayam))
     }
 }
